b2cd5a4692975864c39717053db9734b978bd64e
ESeNonFossiIo <esenonfossiio@gmail.com>
2015-12-04 22:18:25 +0100

    add dynamic libraries

--- cxx/include/matrix.h
+++ cxx/include/matrix.h
@@ -1,98 +1,91 @@
+#ifndef __hungarian_matrix_h__
+#define __hungarian_matrix_h__
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
 #include <map>
 #include <iostream>
 
-template <typename T>
-class hungarian_matrix
+/**
+ *  Matrix Class is a generic structure to store data in matrix form
+ */
+class Matrix
 {
-private:
-  size_t n_row;
-  size_t n_col;
-  std::vector< std::vector<T> > m;
-  std::map<int, size_t> map1;
-  std::map<int, size_t> map2;
-public:  
-  hungarian_matrix()
-  {
-    m.resize(1);
-    m[0].resize(1);
-    n_row = 0;
-    n_col = 0;
-  }
- 
-  void insert(int i, int j, T x)
-  {
-    // update the index maps (which translate each input label 
-    // into a matrix index) and resize the matrix
-    if(!map2.count(j))
-    {
-      // add new index
-      size_t n = map2.size();
-      map2[j] = n;
-      // add a column to the matrix
-      ++n_col;
-      for(size_t k=0; k<m.size(); ++k) m[k].resize(n_col);
-    }
-    if(!map1.count(i)) 
-    {  
-      // add new index
-      size_t n = map1.size();
-      map1[i] = n;
-      // add a row to the matrix
-      ++n_row;
-      m.resize(n_row);
-      m[n_row-1].resize(n_col);
-    }
-    // fill the matrix
-    m[map1[i]][map2[j]] = x;
-  }
+  public:
+    /**
+     * Constructor takes the number of rows and coloumns
+     */
+    Matrix (unsigned int rows, unsigned int cols);
 
-  size_t row_map(int i) 
-  {
-    if(!map1.count(i))
-    {
-      std::cerr << "hungarian_matrix: row_map() couldn't find label " << i << std::endl;
-      exit(1);
-    }
-    return map1[i];
-  }
+    /**
+     *  Add one to the element in position @p i,  @p j
+     */
+    void
+    add_one (unsigned int i, unsigned int j);
 
-  size_t column_map(int i) 
-  {
-    if(!map2.count(i))
-    {
-      std::cerr << "hungarian_matrix: column_map() couldn't find label " << i << std::endl;
-      exit(1);
-    }
-    return map2[i];
-  }
+  private:
+    /**
+     *  Matrix
+     */
+    std::vector<std::vector<unsigned int> > mat;
 
-  T operator() (int i, int j)
-  {
-    row_map(i);
-    column_map(j);
-    return m[map1[i]][map2[j]];
-  }
+    /**
+     *  Number of rows
+     */
+    unsigned int n_row;
 
-  void row_swap(int i, int j)
-  {
-    row_map(i);
-    row_map(j);
-    int tmp = map1[i];
-    map1[i] = map1[j];
-    map1[j] = tmp;
-  }
-
-  void column_swap(int i, int j)
-  {
-    column_map(i);
-    column_map(j);
-    int tmp = map2[i];
-    map2[i] = map2[j];
-    map2[j] = tmp;
-  }
-
+    /**
+     *  Number of coloumns
+     */
+    unsigned int n_col;
 };
 
+/**
+ * HungarianMatrix specializes Matrix to deal with the problem of maximizing
+ * the trance
+ */
+template<typename T>
+class HungarianMatrix : public Matrix
+{
+  public:
+    HungarianMatrix(unsigned int rows, unsigned int cols);
+
+    void row_swap(unsigned int i, unsigned int j);
+    void col_swap(unsigned int i, unsigned int j);
+
+  private:
+    std::map<unsigned int, unsigned int> map1;
+    std::map<unsigned int, unsigned int> map2;
+};
+
+
+//
+//   T operator() (int i, int j)
+//   {
+//     row_map(i);
+//     column_map(j);
+//     return m[map1[i]][map2[j]];
+//   }
+//
+//   void row_swap(int i, int j)
+//   {
+//     row_map(i);
+//     row_map(j);
+//     int tmp = map1[i];
+//     map1[i] = map1[j];
+//     map1[j] = tmp;
+//   }
+//
+//   void column_swap(int i, int j)
+//   {
+//     column_map(i);
+//     column_map(j);
+//     int tmp = map2[i];
+//     map2[i] = map2[j];
+//     map2[j] = tmp;
+//   }
+//
+// };
+
+#endif
