6ce655791853a6ed9c4d2fb90f5209394edcf51e
ESeNonFossiIo <esenonfossiio@gmail.com>
2015-12-09 12:43:59 +0100

    tests

---
+++ cxx/include/sequence_problem.h
@@ -0,0 +1,19 @@
+#ifndef __sequence_problem_h__
+#define __sequence_problem_h__
+
+#include "matrix.h"
+
+/**
+ *  TODO:
+ */
+class SequenceProblem
+{
+public:
+  /**
+   *
+   */
+   SequenceProblem();
+
+};
+
+#endif
---
+++ cxx/tests/matrix_06.cc
@@ -0,0 +1,45 @@
+/*
+ * Description
+ * Author: Luca Ponzoni and Mauro Bardelloni
+ * date: 2015-11-23
+ * time: 18:34:45
+ * test: Hmatrix copy constructor and first_step
+ */
+
+#include "matrix.h"
+#include "utilities.h"
+
+int main(int argc, char *argv[])
+{
+  using namespace utilities;
+
+  HMatrix m;
+  char c[4]= {'A', 'B', 'C', 'D'};
+  for (unsigned int i=0; i<3; ++i)
+    for (unsigned int j=0; j<3; ++j)
+      m.add_an_element(c[i],c[j]);
+  m.add_an_element(c[1],c[1]);
+  m.add_an_element(c[0],c[1]);
+  m.add_an_element(c[0],c[1]);
+  m.add_an_element(c[1],c[0]);
+  m.add_an_element(c[0],c[1]);
+  m.add_an_element(c[0],c[1]);
+  m.add_an_element(c[0],c[1]);
+  m.add_an_element(c[1],c[1]);
+  m.add_an_element(c[2],c[1]);
+  m.add_an_element(c[3],c[1]);
+  HMatrix n(m);
+
+  print_msg("Original matrix:");
+  m.update();
+  std::cout << m;
+
+  m.first_step();
+  m.first_step(false);
+  std::cout << m;
+
+  m.second_step();
+  std::cout << m;
+
+  m.status_mask();
+}
